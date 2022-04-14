0ff6ab7c4a1facc3797c2058776cadc2095ac9fb
Vitalyi Horbovyi <horbovyivitalyi@gmail.com>
2022-04-15 01:21:01 +0300

    Created administrator side

---
+++ page_uploader/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Administrator from './components/Administrator/Administrator'
+
+function App() {
+    return (
+        <div>
+            <Administrator/>
+        </div>
+    );
+}
+
+export default App;
---
+++ page_uploader/src/components/Administrator/Administrator.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import './Administrator.css'
+
+const Administrator = () => {
+    const [selectedImages, setSelectedImages] = useState([]);
+    const [imagesNames, setImagesNames] = useState([])
+    
+    const onSelectFile = (event) => {
+        const selectedFiles = event.target.files;
+        const selectedFilesArray = Array.from(selectedFiles);
+        const imagesArray = selectedFilesArray.map((file) => {
+            return URL.createObjectURL(file);
+        });
+        const imagesNames = selectedFilesArray.map((file) => {
+            return file.name;
+        });
+        setImagesNames((previousNames) => previousNames.concat(imagesNames));
+        setSelectedImages((previousImages) => previousImages.concat(imagesArray));
+    };
+    
+    return(
+        <div className="container">
+            <input 
+                type="file" 
+                id="file-input" 
+                accept="image/png, image/jpeg, image/webp" 
+                onChange={onSelectFile} 
+                onClick={(event) => { event.target.value = '' }}
+                multiple
+            />
+            <label htmlFor="file-input">
+                <i className="fas fa-upload"/>
+                &nbsp; Choose a photo
+            </label>
+            <p id="num-of-files">Files Selected : {selectedImages.length}</p>
+            <div className="images-container">
+                {selectedImages && selectedImages.map((image, index) => {
+                    return (
+                        <figure key={index}>
+                            <div>
+                                <img src={image} alt={imagesNames[index].split('.')[0]}/>
+                                <figcaption>{imagesNames[index]}</figcaption>
+                            </div>
+                        </figure>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default Administrator;
